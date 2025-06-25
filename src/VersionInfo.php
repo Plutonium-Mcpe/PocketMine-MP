075d86e38cd2b8723c52984cfc3494e571a9537e
ShockedPlot7560 <no-reply@tchallon.fr>
2025-06-25 12:06:31 +0200

    Release 4.40.2

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -31,7 +31,7 @@ use function str_repeat;
 
 final class VersionInfo{
 	public const NAME = "PocketMine-MP";
-	public const BASE_VERSION = "4.40.1";
+	public const BASE_VERSION = "4.40.2";
 	public const IS_DEVELOPMENT_BUILD = false;
 	public const BUILD_CHANNEL = "stable";
 
