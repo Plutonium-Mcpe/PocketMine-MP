67ad1e5905d05cfd75d01c18ac2878e449b9a518
ShockedPlot7560 <no-reply@tchallon.fr>
2025-08-04 17:25:18 +0200

    upgrade for mc 1.21.100

--- src/network/mcpe/handler/PreSpawnPacketHandler.php
+++ src/network/mcpe/handler/PreSpawnPacketHandler.php
@@ -109,6 +109,7 @@ class PreSpawnPacketHandler extends PacketHandler{
 				Uuid::fromString(Uuid::NIL),
 				false,
 				false,
+				false,
 				new NetworkPermissions(disableClientSounds: true),
 				[],
 				0
