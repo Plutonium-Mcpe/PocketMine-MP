a2fef63e075874fa31fab38fc0e5d3e2f3d813fd
Bastien <91474741+xEditedNewt@users.noreply.github.com>
2026-01-14 19:18:29 +0100

    Force update workflow permissions

--- src/VersionInfo.php
+++ src/VersionInfo.php
@@ -31,7 +31,7 @@ use function str_repeat;
 
 final class VersionInfo{
 	public const NAME = "PocketMine-MP";
-	public const BASE_VERSION = "5.113.8";
+	public const BASE_VERSION = "5.113.9";
 	public const IS_DEVELOPMENT_BUILD = false;
 	public const BUILD_CHANNEL = "stable";
 	public const GITHUB_URL = "https://github.com/pmmp/PocketMine-MP";
